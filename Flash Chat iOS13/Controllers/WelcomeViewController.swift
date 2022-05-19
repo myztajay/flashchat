8e486ae43a3c2a8baf2b5133d3197f61bbd57b28
Rafael Fernandez <myztajay@hotmail.com>
2022-05-19 14:14:48 -0400

    added segways and animation to launch screen

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        var charIndex = 0.0
+        for letter in "⚡️FlashChat" {
+            Timer.scheduledTimer(withTimeInterval: 0.1*charIndex, repeats: false) { Timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
     
-
+    
 }
