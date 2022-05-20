60b5d030a7739cfcfe6d84b31008841a33058b57
Rafael Fernandez <myztajay@hotmail.com>
2022-05-19 22:08:31 -0400

    added custom cell and table

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -13,12 +13,18 @@ class ChatViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    var messages: [Message] = [
+        Message(sender: "jummy", body: "sup"),
+        Message(sender: "jai", body: "where the dick pics"),
+        Message(sender: "jummy", body: "on the way!!")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "flashchat"
         navigationItem.hidesBackButton = true
-        
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "ReusableCell")
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -36,3 +42,19 @@ class ChatViewController: UIViewController {
     }
     
 }
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // call out how many rows
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // we call out the type of cell to create
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath)
+        as! MessageCell
+        cell.label?.text = messages[indexPath.row].body
+        return cell
+    }
+}
+
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Rafael Fernandez on 5/19/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImgView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
